de558a0ccd0b3724912300a5f87239f45fb9b355
Deondre <deondre.garcia@yahoo.com>
2022-08-09 09:52:58 -0700

    added RSI

--- indicators.py
+++ indicators.py
@@ -1,54 +1,55 @@
 # All the technical indicators to be used in the strategies.py file
 import numpy as np
+from test_arrays import *
 
 # testing_array = [23, 42, 76, 2, 11]
-testing_array = [
-    10,
-    11,
-    14,
-    16,
-    12,
-    23,
-    42,
-    76,
-    2,
-    11,
-    23,
-    46,
-    43,
-    76,
-    23,
-    78,
-    54,
-    76,
-    32,
-    45,
-    67,
-    32,
-    67,
-    13,
-    9,
-    12,
-    6,
-    12,
-    56,
-    78,
-    45,
-    98,
-    47,
-    32,
-    67,
-    13,
-    9,
-    12,
-    6,
-    12,
-    56,
-    78,
-    45,
-    98,
-    47,
-]
+# testing_array = [
+#     10,
+#     11,
+#     14,
+#     16,
+#     12,
+#     23,
+#     42,
+#     76,
+#     2,
+#     11,
+#     23,
+#     46,
+#     43,
+#     76,
+#     23,
+#     78,
+#     54,
+#     76,
+#     32,
+#     45,
+#     67,
+#     32,
+#     67,
+#     13,
+#     9,
+#     12,
+#     6,
+#     12,
+#     56,
+#     78,
+#     45,
+#     98,
+#     47,
+#     32,
+#     67,
+#     13,
+#     9,
+#     12,
+#     6,
+#     12,
+#     56,
+#     78,
+#     45,
+#     98,
+#     47,
+# ]
 
 
 def simple_moving_average(array, periods):
@@ -60,16 +61,11 @@ def simple_moving_average(array, periods):
 def exponential_moving_average(array, periods):
     """simple moving average with more weight on recent data"""
     if len(array) == periods:  # base case
-        # print(f"len(array): {len(array)}, periods: {np.ceil(((periods - 1) / 2))}")
-        print(f"mean: {np.mean(array)}")
-        # print(array)
         return np.mean(array)
     else:
         wm = 2 / (periods + 1)  # calc weighted multiplier
         price_today = array[-1]
-        print(f"price_today: {price_today}")
         new_array = array[:-1]
-        print(f"new_array: {new_array}")
         # recursive formula
         return price_today * wm + exponential_moving_average(new_array, periods) * (
             1 - wm
@@ -117,11 +113,35 @@ def bollinger_bands(array, periods):
 
 
 def relative_strength_index(array, periods):
-    """standard # of periods is 14"""
-    # avg_gain =
+    """
+    standard # of periods is 14, but fast/minute charts might not be good for RSI
+    """
+    gains = []
+    losses = []
+    start_index = len(array) - periods  # only look back at a certain amount of values
+
+    # separate positie price changes from the negative ones
+    for i in range(start_index, len(array) - 1):
+        price_change = array[i + 1] - array[i]
+        if price_change > 0:
+            gains.append(price_change)
+        else:
+            losses.append(price_change)
+
+    avg_gains = np.mean(gains)
+    avg_losses = np.mean(losses)
+    relative_strength = avg_gains / avg_losses
+    rsi = 100 - (100 / (1 - relative_strength))
+    print(rsi)
+    return rsi
+
+
+# avg_gain =
 
 
 # print(exponential_moving_average(testing_array, 12))
 # print(simple_moving_average(testing_array, 3))
-# print(MACD(testing_array, 12, 26, 9))
-print(bollinger_bands(testing_array, 20))
+# print(f"doge_array: {doge_array}")
+# print(MACD(doge_array, 12, 26, 9))
+# print(bollinger_bands(doge_array, 20))
+relative_strength_index(doge_array, 14)
